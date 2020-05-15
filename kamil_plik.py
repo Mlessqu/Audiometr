137ee13e018f62f43ace3af39ab815cf4b85350e
Kamil <missquprisoner@gmail.com>
2020-05-15 19:48:10 +0200

    cos tam csotam

--- kamil_plik.py
+++ kamil_plik.py
@@ -1,4 +1,4 @@
 #dodalem nowy plik zeby zrobic zmiane w repozytorium
 print("Hello World") #tutaj printuje hello worlda
 #bo wypadałoby w miare opisywac co sie robi
-#lubie mleko
+print("moja wersja")
--- nowy_plik.py
+++ nowy_plik.py
@@ -1,6 +1,1 @@
-print("Nowa czesc kodu ktora cos tam robi")
-
-bhjvauiJ#VKh3quih4vv3
-
-
-print("Inny tekst")
+print
