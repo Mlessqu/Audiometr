e2d26d36aff58c9ab9c6c6401b95276134bd6d90
triqqs <annkal6@st.amu.edu.pl>
2020-06-01 14:50:30 +0200

    kolejna próba ze małymi zmianami

--- interfejs.py
+++ interfejs.py
@@ -8,8 +8,6 @@ root.title("Audiometr")
 root.geometry("900x500")
 root.config(bg="LightSteelBlue1")
 font1 = Font(family='Verdana', size=10)
-fonts = list(font.families())
-print(fonts)
 
 #puste funkcje
 def starting():
@@ -24,19 +22,24 @@ def ending():
     messagebox.showinfo("Audiometr", "End")
 
 #lewa ramka
-left_frame = Frame(root, width=200, height=490, bg='grey')
+left_frame = Frame(root, width=250, height=490, bg='grey95')
 left_frame.grid(row=0, column=0, padx=10, pady=5)
 
 #przyciski
 start_png = PhotoImage(file="START.png")
-# button_start = Button(root, font=font1, text="START", padx=68, pady=50, command=starting).place(x=20, y=20)
-button_start = Button(root, image=start_png, bg='grey', border='0', command=starting).place(x=20, y=20)
+button_start = Button(root, image=start_png, bg='grey95', border='0',
+                      command=starting).place(x=40, y=50)
 
-button1 = Button(root, font=font1, text="SŁYSZĘ", padx=65, pady=50, command=hearing).place(x=20, y=170)
+slysze_png = PhotoImage(file="SŁYSZĘ.png")
+button_slyszę = Button(root, font=font1, image=slysze_png, bg='grey95', border='0',
+                       command=hearing).place(x=30, y=200)
 
-button_stop = Button(root, font=font1, text="STOP", padx=71, pady=50, command=ending).place(x=20, y=320)
+stop_png = PhotoImage(file="STOP.png")
+button_stop = Button(root, font=font1, image=stop_png, bg='grey95', border='0',
+                     command=ending).place(x=50, y=350)
+
 #prawa ramka - tu ma być wykres
-right_frame = Frame(root, width=650, height=490, bg='grey')
+right_frame = Frame(root, width=600, height=490, bg='grey95')
 right_frame.grid(row=0, column=1, padx=10, pady=5)
 
 root.mainloop()
