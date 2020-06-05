2d7d1d67d0db52a07b40eadb5ff8e31b4f5e523b
antykwariat <domjus1@st.amu.edu.pl>
2020-06-05 03:05:52 +0200

    Gotowy wykres, pojawia się tam gdzie trzeba.

--- basic_gui.py
+++ basic_gui.py
@@ -1,10 +1,13 @@
-import tkinter as tk
-from tkinter import ttk
 from tkinter import *
 from tkinter import messagebox
 import audiometr as play
 import time
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
 root = Tk() #okno
 root.title("Audiometr")
 root.geometry("900x500")
@@ -35,7 +38,7 @@ def hearing():
             czasy_slyszalnosci_lewe.append(czas)
         if i >= 7:
             czasy_slyszalnosci_prawe.append(czas)
-        if i==7:
+        if i == 6:
             print("Teraz prawe ucho.")
         i = i + 1
         start = time.time()
@@ -45,17 +48,18 @@ def hearing():
         oktawa.stop()
         czas = koniec - start
         czasy_slyszalnosci_prawe.append(czas)
-
-        plt.plot(czestotliwosci, czasy_slyszalnosci_lewe, label="Lewe ucho", marker="x",
-                     markersize=10)
-        plt.plot(czestotliwosci, czasy_slyszalnosci_prawe, label="Prawe ucho", marker="o", markersize=10)
-        plt.title("Wyniki badania")
+        #wykres z wynikami
+        f, a = plt.subplots()
+        a.plot(czestotliwosci, czasy_slyszalnosci_lewe, label= "Lewe ucho", marker= "o", markersize= 10)
+        a.plot(czestotliwosci, czasy_slyszalnosci_prawe, label= "Prawe ucho", marker= "x", markersize= 10)
+        plt.title("Wyniki badania", size= 20)
         plt.xlabel("Częstotliwość (Hz)")
-        plt.ylabel("Czas (sekundy)")
+        plt.ylabel("Czas usłyszenia (sekundy)")
+        a.grid()
         plt.legend()
-        plt.grid()
-        plt.show()
-
+        canvas = FigureCanvasTkAgg(f, root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=1, padx=10, pady=5)
 
 def ending():
     root.destroy()
@@ -65,15 +69,13 @@ left_frame = Frame(root, width=200, height=490, bg='grey')
 left_frame.grid(row=0, column=0, padx=10, pady=5)
 
 #przyciski
-button_start = Button(root, text="start", padx=75, pady=50,command = starting ).place(x=20, y=20)
-button1 = Button(root, text="słyszę", padx=71, pady=50,command = hearing ).place(x=20, y=170)
-button_stop = Button(root, text="stop ", padx=72, pady=50, command = ending ).place(x=20, y=320)
+button_start = Button(root, text="start", padx=75, pady=50,command = starting).place(x=20, y=20)
+button1 = Button(root, text="słyszę", padx=71, pady=50,command = hearing).place(x=20, y=170)
+button_stop = Button(root, text="stop", padx=72, pady=50, command = ending).place(x=20, y=320)
+
 
 #prawa ramka - tu ma być wykres
+
 right_frame = Frame(root, width=650, height=490, bg='grey')
-right_frame.grid(row=0, column=1, padx=10, pady=5)
-
-
-
 
 root.mainloop()
